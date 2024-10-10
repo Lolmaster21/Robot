7c305f13b860d87c767aa064f3bb405f0be836d1
Panda <93364101+NonspecificPanda@users.noreply.github.com>
2024-10-10 16:02:33 -0600

    Add files via upload

---
+++ cole_bot_remote.ino
@@ -0,0 +1,16 @@
+#include <RF24.h>
+#include <RF24_config.h>
+#include <nRF24L01.h>
+#include <printf.h>
+
+RF24
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
